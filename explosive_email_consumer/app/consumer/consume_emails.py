baf8acf15f260affb386311a9aaa02e7185c54b9
nobody <yzeviw@gmail.com>
2024-11-14 13:41:45 +0200

    added consumer for explosive

---
+++ explosive_email_consumer/app/consumer/consume_emails.py
@@ -0,0 +1,12 @@
+import os
+from email_kafka.kafka_connection.consumer import consume
+import hostage_email_consumer.app.service.email_service as email_service
+
+explosive_topic = os.environ['EXPLOSIVE_TOPIC']
+
+
+def consume_emails():
+    consume(
+        topic=explosive_topic,
+        function=email_service.insert_email,
+    )
---
+++ explosive_email_consumer/app/utils/convert_utils.py
@@ -0,0 +1,21 @@
+from psql.models import Email, Device, Location, ExplosiveSentence
+
+def convert_json_to_email_model(email_json) -> Email:
+    return Email(
+        email_address = email_json['email'],
+        username = email_json['username'],
+        ip_address = email_json['ip_address'],
+        email_date = email_json['created_at'],
+        device = Device(
+            device_id = email_json['device_info']['device_id'],
+            os = email_json['device_info']['os'],
+            browser = email_json['device_info']['browser']
+        ),
+        location = Location(
+            lat = email_json['location']['latitude'],
+            lon = email_json['location']['longitude'],
+            city = email_json['location']['city'],
+            country = email_json['location']['country']
+        ),
+        hostage_sentences = [ExplosiveSentence(sentence = sentence) for sentence in email_json['sentences']],
+    )
