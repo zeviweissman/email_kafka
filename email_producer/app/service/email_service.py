d972217667c7c76c89da347ef7802267dfa31cb7
nobody <yzeviw@gmail.com>
2024-11-14 14:39:28 +0200

    added function to rearrange sentences order

--- email_producer/app/service/email_service.py
+++ email_producer/app/service/email_service.py
@@ -5,7 +5,9 @@ def produce_email(email):
     email_producer.all_email_producer(email)
 
     if u.email_contains_hostage(email['sentences']):
-        email_producer.hostage_email_producer(email)
+        processed_email = {**email, 'sentences': u.move_hostage_sentences_to_the_top(email['sentences'])}
+        email_producer.hostage_email_producer(processed_email)
 
     if u.email_contains_explosive(email['sentences']):
+        processed_email = {**email, 'sentences': u.move_explosive_sentences_to_the_top(email['sentences'])}
         email_producer.explosive_email_producer(email)
--- email_producer/app/utils/email_utils.py
+++ email_producer/app/utils/email_utils.py
@@ -1,7 +1,24 @@
+from functools import reduce
 from typing import List
 
-def email_contains_hostage(sentences_list: List[str]):
-    return "hostage" in "".join(sentences_list)
 
-def email_contains_explosive(sentences_list: List[str]):
-    return "explosi" in "".join(sentences_list)
+def sentences_contain_word(sentences_list: List[str], word: str) -> bool:
+    return any([word in string for string in sentences_list])
+
+def email_contains_hostage(sentences_list: List[str]) -> bool:
+    return sentences_contain_word(sentences_list, "hostage")
+
+
+def email_contains_explosive(sentences_list: List[str]) -> bool:
+    return sentences_contain_word(sentences_list, "explosi")
+
+
+def move_sentence_by_word(sentences_list: List[str], word: str) -> List[str]:
+    return reduce(lambda start, next: [next] + start if word in next else start + [next] ,sentences_list, [])
+
+def move_hostage_sentences_to_the_top(sentences_list: List[str]) -> List[str]:
+    return move_sentence_by_word(sentences_list, "hostage")
+
+
+def move_explosive_sentences_to_the_top(sentences_list: List[str]) -> List[str]:
+    return move_sentence_by_word(sentences_list, "explosi")
